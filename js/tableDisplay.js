bda4239016bb5ea9a6e43f9438a005a242d2fdae
ErickCh8023 <eechaparrom@correo.udistrital.edu.co>
2022-09-29 17:40:12 -0500

    version implementando JS

---
+++ js/tableDisplay.js
@@ -0,0 +1,10 @@
+function listenerBoton(){
+    const boton = document.querySelector("#Enviar");
+    boton.addEventListener("click", mostrarNum);
+}
+
+function mostrarNum() {
+    let p1 = document.createElement('p');
+}
+
+window.onload = listenerBoton;
