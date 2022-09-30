0901a529db2380c02ce81f587fd13ef29bdc9bd9
ErickCh8023 <eechaparrom@correo.udistrital.edu.co>
2022-09-29 19:30:51 -0500

    Problema con JS

--- js/tableDisplay.js
+++ js/tableDisplay.js
@@ -1,10 +1,72 @@
+//Listener del botón
 function listenerBoton(){
-    const boton = document.querySelector("#Enviar");
-    boton.addEventListener("click", mostrarNum);
+    const boton = document.querySelector("#enviar");
+    boton.addEventListener("click", metodoRaices);
 }
 
-function mostrarNum() {
-    let p1 = document.createElement('p');
+function metodoRaices(){
+
+    alert("Has dado click")
+
+    let errorDeseado = Number(document.querySelector(".inputErr")); //Error deseado por el usuario
+    let errorActual = 100.0; // Error actual de 100%
+    let Xo = Number(document.querySelector(".inputXo")); //Punto inicial //2
+    let i = 1; //Iteraciones
+
+    alert("Antes del ciclo")
+
+    //Ciclo iterativo
+    while (errorActual > errorDeseado) {
+
+        alert("entra al ciclo")
+
+        console.log("Iteracion: ", i);
+        defFun(Xo);
+        dxFun(Xo);
+        dx2Fun(Xo);
+
+        let Xi = SigXi(Xo); //1,6
+        if(i != 1){
+            Error(Xi, Xo);
+        }
+
+        Xo = Xi; //Xo = 1,6
+        i++;
+    }
+
+    //Evaluar la funcion y sus derivadas
+
+    // f(x) = (x-3)(x-1)^2
+    // f(x) = x^3 - 5x^2 + 7x - 3
+    function defFun(Xo){
+        return Math.pow(Xi,3) - 5 * Math.pow(Xi,2) + 7*Xi -3;
+    }
+
+    // f'(x) = 3x^2 - 10x + 7
+    function dxFun(Xo) {
+        return 3 * Math.pow(Xi,2) - 10 * Xi + 7;
+    }
+
+    //f''(x) = 6x - 10
+    function dx2Fun(Xo) {
+        return 6 * Xi - 10;
+    }
+
+    //Calcular la siguiente raiz
+    function SigXi(Xo){
+        return(
+            Xo - (defFun(Xo) * dxFun(Xo))/
+            ((Math.pow(dxFun(Xo),2)) - (defFun(Xo) * dx2Fun(Xo)))
+        );
+    }
+
+    //Calcular el Error %
+    // Error = ((Xi - Xo)/Xi) * 100
+    function Error(Xi, Xo){
+        return Math.abs(((Xi - Xo) / Xi) * 100);
+    }
+
+    
 }
 
 window.onload = listenerBoton;
